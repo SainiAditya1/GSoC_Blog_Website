be00b70f5db6ed73b96293dabdcbf79c2db796b9
Aditya Saini <adityasaini10012001@outlook.com>
2024-05-05 10:44:34 +0530

    added code for Blog website
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Blog Website For All my GSoC Blogs
---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "Aditya",
+  url: "https://adityasaini.tech",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Aditya",
+  links: {
+    mail: "mailto:Adityasaini10012001@gmail.com",
+    twitter: "https://twitter.com/Adityas19919164",
+    github: "https://github.com/sainiaditya1",
+    personalSite: "https://adityasaini.tech",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
